12fee53e806c5f1122c5fa049855b10dce8bcfee
LilacLeopard <156850911+LilacLeopard@users.noreply.github.com>
2025-05-02 18:43:24 -0400

    move installation script for vimrc to vim-config folder

--- vim-config/install.sh
+++ vim-config/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # copying the .vimrc from the repository into a new .vimrc in the user's home directory
-# this assumes that the user is in the main part of the directory where this script is located
-cp vim-config/.vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 echo "Created .vimrc from the file in repository"
 
 # checks if Vundle.vim doesn't exist yet
